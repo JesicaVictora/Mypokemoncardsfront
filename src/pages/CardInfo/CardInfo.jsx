add6813cdf14f9756de7e52d12bf5baf7b70364b
jesicavictora <jesicavictora@gmail.com>
2021-04-20 12:18:00 +0200

    Cambio en la logica de CardInfo para las cartas que no tienen precio

--- src/pages/CardInfo/CardInfo.jsx
+++ src/pages/CardInfo/CardInfo.jsx
@@ -5,6 +5,7 @@ import {useParams} from 'react-router-dom';
 
 export function CardInfo (){
     const [card, setCard] = useState([]);
+    const [prices, setPrices]= useState([]);
     const {cardId}=useParams();
 
       
@@ -12,21 +13,28 @@ export function CardInfo (){
         API.get('v2/cards/'+cardId).then((res)=> { 
             
            setCard(res.data.data);
+
+           if(res.data.data.tcgplayer){
+            setPrices(res.data.data.tcgplayer.prices);
+           };
+          
         
          });  
     }
     useEffect(getCard, []); 
-
+/*
     useEffect(() => {
         console.log(card);
+        console.log(prices);
       
-      }, [card]);
+      }, [card]);*/
 
 
       return(
             <div>
              { card.images && 
                 <div className="container">
+                    
                       <img src={card.images.small}></img>
            
              
@@ -34,24 +42,29 @@ export function CardInfo (){
                         <h1> {card.name}</h1>
                         <p>Set: {card.set.name}</p>
                         <p>Prices:</p>
-
                     
-                    { card.tcgplayer.prices.normal &&  
+                    { prices=="" &&  
+                        <div>
+                            <p>Price not found</p> <br/>
+                        </div>
+                    }
+                    
+                    {prices.normal &&  
                         <div>
                             <p>Normal: USD {card.tcgplayer.prices.normal.market}</p> <br/>
                         </div>
                     }
-                    { card.tcgplayer.prices['1stEditionHolofoil'] &&  
+                    { prices['1stEditionHolofoil'] &&  
                         <div>
                             <p>1st Edition Holofoil: USD {card.tcgplayer.prices['1stEditionHolofoil'].market}</p> <br/>
                         </div>
                     }
-                    { card.tcgplayer.prices.holofoil &&  
+                    { prices.holofoil &&  
                         <div>
                             <p>Holo Foil: USD {card.tcgplayer.prices.holofoil.market}</p> <br/>
                         </div>
                     }
-                    { card.tcgplayer.prices.reverseHolofoil &&  
+                    { prices.reverseHolofoil &&  
                         <div>
                             <p>Reverse Holo: USD {card.tcgplayer.prices.reverseHolofoil.market}</p> <br/>
                         </div>
